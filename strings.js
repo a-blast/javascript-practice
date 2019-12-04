35bf32ef1188d764e1d59879626b4fa015a46f9b
Austin Armstrong <me@austinarmstrong.dev>
2019-12-04 11:41:17 -0500

    Completes 2.3

--- strings.js
+++ strings.js
@@ -1,16 +1,18 @@
+const _ = require('underscore');
+
 //2.1
 // find anagram indices
 
 const problem1_brute = (inputWord, inputString) =>
 	inputString
-	.split('')
-	.slice(0, inputString.length - inputString.length % inputWord.length)
-	.reduce((agg, val, index) => {
-		const stringToTest = inputString.split('').slice(index, index + inputWord.length).join('');
-		const match = stringToTest === inputWord || stringToTest === inputWord.split('').reverse().join('');
-		match && agg.push(index);
-		return agg;
-	}, []);
+		.split('')
+		.slice(0, inputString.length - inputString.length % inputWord.length)
+		.reduce((agg, val, index) => {
+			const stringToTest = inputString.split('').slice(index, index + inputWord.length).join('');
+			const match = stringToTest === inputWord || stringToTest === inputWord.split('').reverse().join('');
+			match && agg.push(index);
+			return agg;
+		}, []);
 
 console.log(problem1_brute('ab', 'abxaba'));
 
@@ -22,48 +24,71 @@ const problem2_brute = (inputWords) => {
 
 	const pairs = inputWords.reduce((acc, word1, index1) => {
 		const wordPalindromes = inputWords
-			.map((word2, index2) => (isPalindrome(word1 + word2) ? [index1, index2] : false))
+			.map((word2, index2) => (isPalindrome(word1 + word2) ? [ index1, index2 ] : false))
 			.filter((val) => val);
-		acc.push(...wordPalindromes)
-		return acc
+		acc.push(...wordPalindromes);
+		return acc;
 	}, []);
 
-	return pairs
+	return pairs;
 };
 
-console.log(problem2_brute(["code", "edoc", "da", "d"]))
+console.log(problem2_brute([ 'code', 'edoc', 'da', 'd' ]));
 
 const problem2 = (inputWords) => {
 	const isPalindrome = (word) => word === word.split('').reverse().join('');
 
 	// Assuming dictionary words are unique..
 	const dictionary = inputWords.reduce((acc, word, index) => {
-		acc[word] = index
-		return acc
-	}, {})
+		acc[word] = index;
+		return acc;
+	}, {});
 
 	const pairs = inputWords.reduce((acc, word, wordIndex) => {
-		const wordArr = word.split('')
+		const wordArr = word.split('');
 
 		wordArr.some((_, letterIndex) => {
-			const prefixToCheck = wordArr.slice(0, letterIndex).join('')
-			const suffixToCheck = wordArr.slice(letterIndex).reverse().join('')
+			const prefixToCheck = wordArr.slice(0, letterIndex).join('');
+			const suffixToCheck = wordArr.slice(letterIndex).reverse().join('');
 			if (isPalindrome(prefixToCheck)) {
-				suffixToCheck in dictionary && dictionary[suffixToCheck] !== wordIndex &&
-					acc.push([wordIndex, dictionary[suffixToCheck]])
-				prefixToCheck in dictionary && dictionary[prefixToCheck] !== wordIndex &&
-					acc.push([wordIndex, dictionary[prefixToCheck]])
+				suffixToCheck in dictionary &&
+					dictionary[suffixToCheck] !== wordIndex &&
+					acc.push([ wordIndex, dictionary[suffixToCheck] ]);
+				prefixToCheck in dictionary &&
+					dictionary[prefixToCheck] !== wordIndex &&
+					acc.push([ wordIndex, dictionary[prefixToCheck] ]);
 			} else {
 				// exit the loop, if the prefix is not a substring there is no need to continue to iterate
-				return true
+				return true;
 			}
-
 		});
 
-		return acc
-	}, [])
+		return acc;
+	}, []);
+	return pairs;
+};
 
-	return pairs
-}
+console.log(problem2([ 'code', 'edoc', 'da', 'd' ]));
 
-console.log(problem2(["code", "edoc", "da", "d"]))
+// 2.3
+// print zigzag form
+
+const _ = require('underscore');
+const problem3 = (input, k) =>
+	_.range(k).reduce((acc, _v, offset) => {
+		// Extract the pattern by shifting & running the modulo at specififed indexes
+		const line = input
+			.split('')
+			.map(
+				(val, index) =>
+					(index - offset) % (2 * k - 2) === 0 || (index - (2 * k - 2 - offset)) % (2 * k - 2) === 0
+						? val
+						: ' '
+			);
+		line.push('\n');
+		acc = acc + line.join('');
+		return acc;
+	}, '');
+
+console.log(problem3('thisisazigzag', 4));
+console.log(problem3('thisisanotherzigzag', 5));
